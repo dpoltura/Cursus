b7b184cccaa79b19bc89a8ef067f44d7dcdabc73
Donovan Polturat <dpoltura@2A4.42angouleme.fr>
2023-11-03 16:14:32 +0100

    up

--- Cursus/libft/ft_strlcat.c
+++ Cursus/libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:37:54 by dpoltura          #+#    #+#             */
-/*   Updated: 2023/11/03 12:35:57 by dpoltura         ###   ########.fr       */
+/*   Updated: 2023/11/03 16:05:21 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	int	j;
+	size_t	j;
+	size_t	full_len;
 
+	if (size == 0 && dst == NULL)
+		return (0);
 	i = 0;
 	j = 0;
+	while (dst[i] != '\0' && i < size)
+        	i++;
+        full_len = i + ft_strlen(src);
 	if (size != 0)
 	{
-		while (dst[i] != '\0')
-			i++;
+		i = ft_strlen(dst);
 		while (src[j] != '\0' && i < size - 1)
 		{
 			dst [i] = src [j];
@@ -32,5 +37,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		}
 		dst[i] = '\0';
 	}
-	return (ft_strlen(dst) + ft_strlen(src));
+	return (full_len);
 }
