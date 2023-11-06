b856d82c218639176ba6c5d47718fa7b29acc731
Donovan Polturat <dpoltura@1C4.42angouleme.fr>
2023-11-06 17:03:04 +0100

    up

--- Cursus/libft/ft_split.c
+++ Cursus/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 09:29:30 by dpoltura          #+#    #+#             */
-/*   Updated: 2023/11/06 11:44:25 by dpoltura         ###   ########.fr       */
+/*   Updated: 2023/11/06 16:28:21 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,98 +17,25 @@
 int	ft_count_word(const char *s, char c)
 {
 	int	i;
-	int	count_word;
+	int	n_word;
 
 	i = 0;
-	count_word = 0;
+	n_word = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c)
+		while (s[i] == c)
 			i++;
-		else
-		{
-			count_word++;
+		while (s[i] != c && s[i] != '\0')
 			i++;
-		}
+		while (s[i] == c)
+                        i++;
+		n_word++;
 	}
-	return (count_word);
-}
-
-int	ft_word_len(const char *s, char c, int start)
-{
-	int	i;
-
-	i = 0;
-	while (s[start] != c)
-	{
-		start++;
-		i++;
-	}
-	return (i);
-}
-
-void	*ft_alloc_split(char **split, const char *s, char c)
-{
-	int	i;
-	int	start;
-	int	count_word;
-	int	word_len;
-
-	i = 0;
-	start = 0;
-	count_word = ft_count_word(s, c);
-	word_len = ft_word_len(s, c, start);
-	while (i < count_word)
-	{
-		split[i] = malloc(sizeof(char *) * (word_len + 1));
-		if (split[i] == NULL)
-			return (NULL);
-		start += word_len;
-		i++;
-	}
-	return (0);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	int	i;
-	int	j;
-	int	k;
-	char	**split;
-	int	count_word;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	count_word = ft_count_word(s, c);
-	split = malloc(sizeof(char **) * (count_word + 1));
-	if (split == NULL)
-		return (NULL);
-	ft_alloc_split(split, s, c);
-	while (count_word > 0)
-	{
-		if (s[i] == c)
-		{
-			count_word--;
-			i++;
-			j++;
-			k = 0;
-		}
-		if (s[i] == '\0' || i > 11)
-			return (split);
-		split[j][k] = s[i];
-		i++;
-		k++;
-	}
-	return (split);
+	return (n_word);
 }
 
 int	main(void)
 {
-	char	**split;
-
-	split = ft_split("Hello|World|wefwefwefwfe|wefwef|dwefwef", '|');
-	printf("%s\n%s\n%s\n", split[0], split[1], split[2]);
-	free(split);
+	printf("%d\n", ft_count_word("wefwefwe|||||||||wefwef", '|'));
 	return (0);
 }
