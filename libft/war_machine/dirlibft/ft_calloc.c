d29b1b0d48cae60027b47fbcf8834f972da579a9
Donovan Polturat <dpoltura@2A4.42angouleme.fr>
2023-11-07 17:00:37 +0100

    up

---
+++ libft/war_machine/dirlibft/ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoltura <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 15:51:30 by dpoltura          #+#    #+#             */
+/*   Updated: 2023/11/06 09:32:46 by dpoltura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*alloc;
+	size_t	i;
+
+	i = 0;
+	alloc = malloc(size * nmemb);
+	if (alloc == NULL)
+		return (NULL);
+	while (i < nmemb * size)
+	{
+		alloc[i] = 0;
+		i++;
+	}
+	return ((void *)alloc);
+}
---
+++ libft/war_machine/dirlibft/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoltura <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 09:44:13 by dpoltura          #+#    #+#             */
+/*   Updated: 2023/11/03 10:10:04 by dpoltura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	char		*p1;
+	const char	*p2;
+
+	i = 0;
+	p1 = dest;
+	p2 = src;
+	if (!dest && !src)
+		return (NULL);
+	if (src >= dest)
+	{
+		while (i < n)
+		{
+			p1[i] = p2[i];
+			i++;
+		}
+	}
+	else
+		while (n--)
+			p1[n] = p2[n];
+	return (dest);
+}
