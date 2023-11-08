7274f356de0edca79905b627a070e1cdf2328034
dpoltura <don.polturat@gmail.com>
2023-11-08 11:02:27 +0100

    up

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -21,10 +21,10 @@ int	ft_atoi(const char *nptr)
 	k = 0;
 	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
-	while (nptr[i] == '+' || nptr[i] == '-')
+	if (nptr[i] == '+' || nptr[i] == '-')
 	{
 		if (nptr[i] == '-')
-			j *= -1;
+			j = -1;
 		i++;
 	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
