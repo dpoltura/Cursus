f5d2d74522002b9aca964b73c205aa1648255220
dpoltura <don.polturat@gmail.com>
2023-11-08 10:35:25 +0100

    up

--- libft/war_machine/dirlibft/ft_strnstr.c
+++ libft/war_machine/dirlibft/ft_strnstr.c
@@ -17,22 +17,19 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	j = 0;
-	while (little[i] != '\0')
-		i++;
-	if (i == 0)
+	if ((!big || !little) && len == 0)
+		return (NULL);
+	if (!little[0])
 		return ((char *)big);
 	i = 0;
-	while (i < len)
+	while (big[i] && i < len)
 	{
-		while (little[j] == big[i])
-		{
-			i++;
+		j = 0;
+		while (big[i + j] && little[j] && \
+			i + j < len && big[i + j] == little[j])
 			j++;
-			if (i <= len && little[j] == '\0')
-				return ((char *)big + (i - j));
-		}
+		if (!little[j])
+			return ((char *)(big + i));
 		i++;
 	}
 	return (NULL);
