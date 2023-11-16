e5bdd5423f44ce219356f8964039a920ef4f0126
dpoltura <dpoltura@2A1.42angouleme.fr>
2023-11-16 09:52:06 +0100

    up

--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,13 +6,17 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 08:59:26 by dpoltura          #+#    #+#             */
-/*   Updated: 2023/11/16 09:01:33 by dpoltura         ###   ########.fr       */
+/*   Updated: 2023/11/16 09:36:40 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-​int    ft_printf( const char *restrict format, ... );
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int ft_printf( const char *restrict format, ... );
 
 #endif
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,36 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/16 08:58:56 by dpoltura          #+#    #+#             */
-/*   Updated: 2023/11/16 09:50:36 by dpoltura         ###   ########.fr       */
+/*   Created: 2023/11/16 09:34:14 by dpoltura          #+#    #+#             */
+/*   Updated: 2023/11/16 09:51:28 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int ft_printf(const char *restrict format, ... )
+int main(void)
 {
-    va_list args;
-    va_start(args, format);
-    int i;
-
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1) == 'c')
-        {
-            i = va_arg(args, int);
-            format += 2;
-            printf("%c", i);
-            if (!(*format))
-                break;
-        }
-        printf("%c", *format);
-        format++;
-    }
-    va_end(args);
+    ft_printf("coucou %c ca va", 's');
     return (0);
 }
