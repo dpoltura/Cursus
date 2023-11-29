320c06ee37e1c3bdda42283a098e07be32dcf125
dpoltura <don.polturat@gmail.com>
2023-11-29 14:18:50 +0100

    up

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 09:16:20 by dpoltura          #+#    #+#             */
-/*   Updated: 2023/11/29 13:53:25 by dpoltura         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:13:55 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int    main(void)
 
     i = 0;
     fd = open("test.txt", O_RDONLY);
-    while (i < 13)
+    while (i < 3)
     {
         line = get_next_line(fd);
         printf("%s\n", line);
