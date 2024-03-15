0cfc34225c6e70be6474b4792c811f3d0cf18c30
dpoltura <dpoltura@1A7.42angouleme.fr>
2024-03-15 13:34:35 +0100

    up

--- minishell/directory.c
+++ minishell/directory.c
@@ -6,27 +6,51 @@
 /*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 10:58:46 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/03/15 11:41:14 by dpoltura         ###   ########.fr       */
+/*   Updated: 2024/03/15 13:24:33 by dpoltura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	if_back_directory(char **split_input)
+{
+	int	i;
+	char	dir[1024];
+
+	i = 1;
+	while (split_input[i])
+	{
+		if (ft_strcmp(split_input[i], ".."))
+			chdir("..");
+		getcwd(dir, sizeof(dir));
+		setenv("PWD", dir, 1);
+		if (split_input[i] && ft_strcmp(split_input[i], "/"))
+			i++;
+		if (!split_input[i])
+			return (1);
+		i++;
+	} 
+	return (0);
+}
+
 int	if_directory(char **split_input)
 {
+	int	i;
 	char	*dir;
 
+	i = 1;
 	if (ft_strcmp(split_input[0], "cd"))
 	{
-		if (ft_strcmp(split_input[1], ".."))
-			chdir("..");
-		else if (access(split_input[1], R_OK) == 0)
+		if (!if_back_directory(split_input))
 		{
-			dir = ft_strdup(getenv("OLDPWD"));
-			dir = ft_strjoin(dir, "/");
-			dir = ft_strjoin(dir, split_input[1]);
-			chdir(dir);
-			free(dir);
+			if (access(split_input[i], R_OK) == 0)
+			{
+				dir = ft_strdup(getenv("PWD"));
+				dir = ft_strjoin(dir, "/");
+				dir = ft_strjoin(dir, split_input[1]);
+				chdir(dir);
+				free(dir);
+			}
 		}
 		return (1);
 	}
